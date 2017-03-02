9252b1ae815866780c69edff856508d9d4a06ffa
Alessandro Tasora <tasora@ied.unipr.it>
2017-03-02 11:55:48 +0100

    Create a piastrella

--- my_example.cpp
+++ my_example.cpp
@@ -1,12 +1,6 @@
 //
-// PROJECT CHRONO - http://projectchrono.org
+// Laura - simulator of road pavements 
 //
-// Copyright (c) 2013 Project Chrono
-// All rights reserved.
-//
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file at the top level of the distribution
-// and at http://projectchrono.org/license-chrono.txt.
 //
 
 // A very simple example that can be used as template project for
@@ -18,6 +12,7 @@
 #include "chrono/assets/ChTexture.h"
 #include "chrono/assets/ChColorAsset.h"
 #include "chrono_irrlicht/ChIrrApp.h"
+#include "chrono/assets/ChPointPointDrawing.h"
 
 // Use the namespace of Chrono
 
@@ -46,7 +41,7 @@ int main(int argc, char* argv[]) {
                          false);  // screen dimensions
 
     // Easy shortcuts to add camera, lights, logo and sky in Irrlicht scene:
-    application.AddTypicalLogo();
+    //application.AddTypicalLogo();
     application.AddTypicalSky();
     application.AddTypicalLights();
     application.AddTypicalCamera(core::vector3df(2, 2, -5),
@@ -62,51 +57,118 @@ int main(int argc, char* argv[]) {
     // 1-Create a floor that is fixed (that is used also to represent the absolute reference)
 
     auto floorBody = std::make_shared<ChBodyEasyBox>(10, 2, 10,  // x, y, z dimensions
-                                                     3000,       // density
+                                                     1000,       // density
                                                      false,      // no contact geometry
                                                      true        // enable visualization geometry
                                                      );
-    floorBody->SetPos(ChVector<>(0, -2, 0));
+    ChVector<> mypos;
+    mypos.Set(0, -1, 0);  // or:   mypos.y() = -2;
+    floorBody->SetPos(mypos);
     floorBody->SetBodyFixed(true);
 
     mphysicalSystem.Add(floorBody);
 
-    // 2-Create a pendulum
+    // 2-Create a piastrella
 
-    auto pendulumBody = std::make_shared<ChBodyEasyBox>(0.5, 2, 0.5,  // x, y, z dimensions
-                                                        3000,         // density
-                                                        false,        // no contact geometry
+    auto piastrellaBody = std::make_shared<ChBodyEasyBox>(0.2, 0.1, 0.2,  // x, y, z dimensions
+                                                        1500,         // density
+                                                        true,        // no contact geometry
                                                         true          // enable visualization geometry
                                                         );
-    pendulumBody->SetPos(ChVector<>(0, 3, 0));
-    pendulumBody->SetPos_dt(ChVector<>(1, 0, 0));
+    piastrellaBody->SetPos(ChVector<>(0, 0.10, 0));
 
-    mphysicalSystem.Add(pendulumBody);
+    mphysicalSystem.Add(piastrellaBody);
 
-    // 3-Create a spherical constraint.
-    //   Here we'll use a ChLinkMateGeneric, but we could also use ChLinkLockSpherical
 
-    auto sphericalLink =
-        std::make_shared<ChLinkMateGeneric>(true, true, true, false, false, false);  // x,y,z,Rx,Ry,Rz constrains
-    ChFrame<> link_position_abs(ChVector<>(0, 4, 0));
+    // Create the spring(s)
 
-    sphericalLink->Initialize(pendulumBody,        // the 1st body to connect
-                              floorBody,           // the 2nd body to connect
-                              false,               // the two following frames are in absolute, not relative, coords.
-                              link_position_abs,   // the link reference attached to 1st body
-                              link_position_abs);  // the link reference attached to 2nd body
+    // vertical, soil:
+    double vert_stiffness = 12500;  // N/m
+    double vert_damping   = 100;    // N/ m/s
 
-    mphysicalSystem.Add(sphericalLink);
+    auto molla1 = std::make_shared<ChLinkSpring>();
+    molla1->Initialize(piastrellaBody, floorBody, false, ChVector<>(-0.10, 0.05, -0.10), ChVector<>(-0.10, 0, -0.10), true);
+    molla1->Set_SpringK(vert_stiffness);
+    molla1->Set_SpringR(vert_damping);
+    auto molla1_vis = std::make_shared<ChPointPointSegment>();  // or..  ChPointPointSpring 
+    molla1->AddAsset(molla1_vis);
+    mphysicalSystem.Add(molla1);
+
+    auto molla2 = std::make_shared<ChLinkSpring>();
+    molla2->Initialize(piastrellaBody, floorBody, false, ChVector<>( 0.10, 0.05, -0.10), ChVector<>( 0.10, 0, -0.10), true);
+    molla2->Set_SpringK(vert_stiffness);
+    molla2->Set_SpringR(vert_damping);
+    auto molla2_vis = std::make_shared<ChPointPointSegment>();  // or..  ChPointPointSpring 
+    molla2->AddAsset(molla2_vis);
+    mphysicalSystem.Add(molla2);
+
+    auto molla3 = std::make_shared<ChLinkSpring>();
+    molla3->Initialize(piastrellaBody, floorBody, false, ChVector<>( 0.10, 0.05,  0.10), ChVector<>( 0.10, 0,  0.10), true);
+    molla3->Set_SpringK(vert_stiffness);
+    molla3->Set_SpringR(vert_damping);
+    auto molla3_vis = std::make_shared<ChPointPointSegment>();  // or..  ChPointPointSpring 
+    molla3->AddAsset(molla3_vis);
+    mphysicalSystem.Add(molla3);
+
+    auto molla4 = std::make_shared<ChLinkSpring>();
+    molla4->Initialize(piastrellaBody, floorBody, false, ChVector<>(-0.10, 0.05,  0.10), ChVector<>(-0.10, 0,  0.10), true);
+    molla4->Set_SpringK(vert_stiffness);
+    molla4->Set_SpringR(vert_damping);
+    auto molla4_vis = std::make_shared<ChPointPointSegment>();  // or..  ChPointPointSpring 
+    molla4->AddAsset(molla4_vis);
+    mphysicalSystem.Add(molla4);
+
+     // horizontal, soil:
+    double hor_stiffness = 12500;  // N/m
+    double hor_damping   = 100;    // N/ m/s
+
+    auto mollaHx = std::make_shared<ChLinkSpring>();
+    mollaHx->Initialize(piastrellaBody, floorBody, false, ChVector<>(0, 0.05, 0), ChVector<>(0.04, 0.05, 0), true);
+    mollaHx->Set_SpringK(hor_stiffness);
+    mollaHx->Set_SpringR(hor_damping);
+    auto mollaHx_vis = std::make_shared<ChPointPointSegment>();  // or..  ChPointPointSpring 
+    mollaHx->AddAsset(mollaHx_vis);
+    mphysicalSystem.Add(mollaHx);
+
+    auto mollaHy = std::make_shared<ChLinkSpring>();
+    mollaHy->Initialize(piastrellaBody, floorBody, false, ChVector<>(0, 0.05, 0), ChVector<>(0, 0.05, 0.04), true);
+    mollaHy->Set_SpringK(hor_stiffness);
+    mollaHy->Set_SpringR(hor_damping);
+    auto mollaHy_vis = std::make_shared<ChPointPointSegment>();  // or..  ChPointPointSpring 
+    mollaHy->AddAsset(mollaHy_vis);
+    mphysicalSystem.Add(mollaHy);
+
+
+
+
+
+
+    // 3- Create a rigid wheel 
+
+    auto wheelBody = std::make_shared<ChBodyEasyCylinder>(0.4, 0.285,  // R, h
+                                                        200,         // density
+                                                        true,        //  contact geometry
+                                                        true          // enable visualization geometry
+                                                        );
+    wheelBody->SetPos(ChVector<>(0, 0.60, 0));
+    wheelBody->SetRot(Q_from_AngX(CH_C_PI_2)); // 90 deg
+
+    mphysicalSystem.Add(wheelBody);
+
 
     // Optionally, attach a RGB color asset to the floor, for better visualization
     auto color = std::make_shared<ChColorAsset>();
     color->SetColor(ChColor(0.2f, 0.25f, 0.25f));
     floorBody->AddAsset(color);
 
+    /*
     // Optionally, attach a texture to the pendulum, for better visualization
     auto texture = std::make_shared<ChTexture>();
     texture->SetTextureFilename(GetChronoDataFile("cubetexture_bluwhite.png"));  // texture in ../data
     pendulumBody->AddAsset(texture);
+    */
+
+
 
     //======================================================================
 
